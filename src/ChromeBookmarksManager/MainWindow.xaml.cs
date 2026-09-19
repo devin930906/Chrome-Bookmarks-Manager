80d760cc4e94fdf20e66da25db593ac38a2892df
devin930906 <51188953+devin930906@users.noreply.github.com>
2026-09-19 19:56:05 +0800

    feat: open bookmark files through reader

--- src/ChromeBookmarksManager/MainWindow.xaml.cs
+++ src/ChromeBookmarksManager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 using System.Windows;
+using ChromeBookmarksManager.Chrome;
 using ChromeBookmarksManager.ViewModels;
+using Microsoft.Win32;
 
 namespace ChromeBookmarksManager;
 
@@ -8,6 +10,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = new MainViewModel();
+        DataContext = new MainViewModel(new ChromeBookmarksReader());
+    }
+
+    private MainViewModel ViewModel => (MainViewModel)DataContext;
+
+    private async void OpenBookmarks_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            FileName = "Bookmarks",
+            Filter = "Chrome Bookmarks file|Bookmarks|JSON files|*.json|All files|*.*",
+            CheckFileExists = true
+        };
+
+        if (dialog.ShowDialog(this) == true)
+        {
+            await ViewModel.LoadBookmarksAsync(dialog.FileName);
+        }
+    }
+
+    private void CancelLoad_Click(object sender, RoutedEventArgs e)
+    {
+        ViewModel.CancelLoad();
     }
 }
